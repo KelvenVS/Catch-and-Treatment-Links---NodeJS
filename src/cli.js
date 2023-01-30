341a5cb3d33c6f64afb6f7630a6eb1116b40b8d9
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 14:40:02 -0300

    add function verify dir and att printFunction

--- src/cli.js
+++ src/cli.js
@@ -4,21 +4,33 @@ import bringfile from './index.js';
 
 const path = process.argv;
 
-function printResult(result){
-  console.log(chalk.yellow('Lista de links'),result);
+function printList(result, identify = ''){
+  console.log(chalk.yellow('Lista de links'),
+  chalk.black.bgGreen(identify),
+  result);
 }
 
 async function processText(pathArguments) {
   const path = pathArguments[2];
 
+  try{
+    fs.lstatSync(path);
+  }catch(fault){
+    if (fault.code === 'ENOENT'){
+      console.log(chalk.red('Arquivo ou diretório não existe'));
+      return;
+    }
+  }
+
   if (fs.lstatSync(path).isFile()){
     const result = await bringfile(pathArguments[2]);
-    printResult(result);
+    printList(result);
+
   } else if (fs.lstatSync(path).isDirectory()) {
     const file = await fs.promises.readdir(path);
     file.forEach(async (nameFile) => {
       const list = await bringfile(`${path}/${nameFile}`);
-      printResult(list);
+      printList(list,nameFile);
     })
     console.log(file);
   }
@@ -26,4 +38,6 @@ async function processText(pathArguments) {
 }
 
 // node src/cli.js  ./arquivos/text.md 
+// node src/cli.js  ./arquivos/ 
+// node src/cli.js  ./arquivo/ 
 processText(path);
