d7672dccac2a77dcfa8161512f2eee55b032f580
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 15:34:20 -0300

    check and validate only on an array

--- src/http-check.js
+++ src/http-check.js
@@ -1,5 +1,5 @@
 function extractLinksArray (arrLinks){
-  return arrLinks.map((objectLink) => Object.values(objectLink))
+  return arrLinks.map((objectLink) => Object.values(objectLink).join())
 }
 
 export default function listValidated (listLinks) {
