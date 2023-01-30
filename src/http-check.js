dbb784f7b1bc8fe1fe8d9c4fb8d12d0c03fc6d81
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 16:19:04 -0300

    treatmentError func

--- src/http-check.js
+++ src/http-check.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extractLinksArray (arrLinks){
   return arrLinks.map((objectLink) => Object.values(objectLink).join())
 }
@@ -5,18 +7,27 @@ function extractLinksArray (arrLinks){
 async function checkStatus (listURLs) {
   const arrStatus = await Promise.all(
     listURLs.map(async (url) => {
-      const response = await fetch(url);
-      return response.status;
+      try {
+        const response = await fetch(url);
+        return response.status;
+      } catch (fault) {
+        return treatmentError(fault);
+      }
     })
   )
   return arrStatus;
 }
 
+function treatmentError (fault){
+  console.log(chalk.red('Algo deu errado',fault));
+}
+
 export default async function listValidated (listLinks) {
   const links = extractLinksArray(listLinks);
   const status = await checkStatus(links);
-  // console.log(status);
-  return status;
-}
 
-// [gatinho salsicha](http://gatinhosalsicha.com.br/)
+  return listLinks.map((object,index) => ({
+    ...object,
+    status: status[index]
+  }))
+}
