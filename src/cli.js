b0f099dbb395e2e158a4a83fce68b22de98fba90
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 14:05:38 -0300

    divide functions

--- src/cli.js
+++ src/cli.js
@@ -4,18 +4,21 @@ import bringfile from './index.js';
 
 const path = process.argv;
 
+function printResult(result){
+  console.log(chalk.yellow('Lista de links'),result);
+}
+
 async function processText(pathArguments) {
   const path = pathArguments[2];
 
   if (fs.lstatSync(path).isFile()){
     const result = await bringfile(pathArguments[2]);
-    console.log(chalk.yellow('Lista de Links'),result);
+    printResult(result);
   } else if (fs.lstatSync(path).isDirectory()) {
     const file = await fs.promises.readdir(path);
     file.forEach(async (nameFile) => {
       const list = await bringfile(`${path}/${nameFile}`);
-      console.log(`${path}/${nameFile}`); // diretory of list array
-      console.log(list); // list array
+      printResult(list);
     })
     console.log(file);
   }
