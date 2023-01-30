4215b0a7f3ef58f06af5591173a22777f9af9a69
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 13:24:08 -0300

    move index to SRC folder

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import chalk from 'chalk';
+// const chalk= require('chalk'); // old-version -- other way
+
+function extractLinks(text){
+  const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  // const capture = text.match(regex);
+  // const capture = regex.exec(text);
+  const capture = [...text.matchAll(regex)];
+  const result = capture.map(capture => ({[capture[1]]: capture[2]}))
+  // console.log(result);
+  return result;
+}
+
+function handlingError(fault) {
+    console.log(fault);
+    throw new Error(chalk.red(fault.code, 'Não há arquivo no diretório'));
+}
+
+// aSync/aWait
+
+async function bringfile(pathFile){
+
+  try{
+    const encoding = 'utf-8';
+    const text = await fs.promises.readFile(pathFile , encoding);
+    console.log(extractLinks(text));
+  } catch (fault) {
+    handlingError(fault);
+  }
+}
+// \[[^\[\]]*?\]
+
+// /\(https?:\/\/[^\s?#.].[^\s]*\)
+
+//groups
+// /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+
+// bringfile('./arquivos/text.md');
+// bringfile('./arquivos/');
+
+export default bringfile;
