747db93407406f1fcc890121811ff2054c3b3d6a
KelvenVS <kelven.serejo@gmail.com>
2023-01-30 15:57:38 -0300

    add fetch()

--- src/http-check.js
+++ src/http-check.js
@@ -2,6 +2,22 @@ function extractLinksArray (arrLinks){
   return arrLinks.map((objectLink) => Object.values(objectLink).join())
 }
 
+async function checkStatus (listURLs) {
+  return listURLs.map(async (url) => {
+    const response = await fetch(url);
+    return response.status;
+  })
+}
+
+// const res = await fetch('https://nodejs.org/api/documentation.json');
+// if (res.ok) {
+//   const data = await res.json();
+//   console.log(data);
+// }
+
 export default function listValidated (listLinks) {
-  return extractLinksArray(listLinks);
+  const links = extractLinksArray(listLinks);
+  const status = checkStatus(links);
+  console.log(status);
+  return status;
 }
