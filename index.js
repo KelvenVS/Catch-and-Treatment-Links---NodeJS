eece2df8e1335463a21f885e362d6da9678678db
KelvenVS <kelven.serejo@gmail.com>
2023-01-29 22:07:43 -0300

    add Library and gitignore

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import chalk from 'chalk';
+
+// const chalk= require('chalk'); // old-version -- other way
+
+console.log(chalk.blue('Hello world!'));
+
 console.log('olá mundo');
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input)');
 console.log(', a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
